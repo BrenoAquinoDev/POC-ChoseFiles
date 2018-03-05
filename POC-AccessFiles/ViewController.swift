a01ed1ee5c3b2d3cff14de02da1508ccc3948b4e
Breno Aquino <breno@m048162.local>
2018-03-05 10:44:23 -0300

    Initial Commit

---
+++ POC-AccessFiles/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POC-AccessFiles
+//
+//  Created by Breno Aquino on 05/03/18.
+//  Copyright © 2018 Breno Aquino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
