2bf03ca7daa121e0e91a4703f6760ed451f851ba
Breno Aquino <breno@M048162.local>
2018-03-05 11:43:14 -0300

    Photo Library, Movie Library

--- POC-AccessFiles/ViewController.swift
+++ POC-AccessFiles/ViewController.swift
@@ -7,19 +7,74 @@
 //
 
 import UIKit
+import MobileCoreServices
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var imageView: UIImageView!
+
+    @IBAction func photoAction(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .photoLibrary
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            self.present(myPickerController, animated: true)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func movieAction(_ sender: Any) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .photoLibrary
+            myPickerController.mediaTypes = [kUTTypeMovie as String]
+            self.present(myPickerController, animated: true)
+        }
     }
+    
+    @IBAction func docAction(_ sender: Any) {
+        
+        
+    }
+    
+    @IBAction func anythingAction(_ sender: Any) {
+        
+        
+    }
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        // Photo
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        
+            self.imageView.image = image
+            
+        }
+        // Movie
+        else if let videoURL = info[UIImagePickerControllerMediaURL]as? NSURL {
+            
+            let asset = AVURLAsset(url: videoURL as URL)
+            let imgGenerator = AVAssetImageGenerator(asset: asset)
+            
+            imgGenerator.appliesPreferredTrackTransform = true
+            
+            let cgImage = try! imgGenerator.copyCGImage(at: CMTimeMake(0, 1), actualTime: nil)
+            let thumbnail = UIImage(cgImage: cgImage)
+            
+            self.imageView.image = thumbnail
+        }
+        
+        self.dismiss(animated: true)
+    }
 }
 
